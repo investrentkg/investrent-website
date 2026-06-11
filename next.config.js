9744e84d1c25fc56d58d71b95549ab4f216585d8
investrentkg <zarzad@investrent.com.pl>
2026-06-11 16:49:49 +0200

    init: next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['sqpepaiqwxginqnglspl.supabase.co', 'investrent.com.pl'],
+  },
+}
+
+module.exports = nextConfig
